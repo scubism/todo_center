6e01a5f141efe47deda16145476e2a5e99abbb7c
HuyNQ <huy@huynq.net>
2016-03-25 20:57:22 +0900

    Fix docker-compose syntax

--- init.sh
+++ init.sh
@@ -9,6 +9,6 @@ echo REACT_TODO_WEB_PORT=$REACT_TODO_WEB_PORT
 # find and replace
 sed -e "s/{{ HOST }}/$HOST/g" \
     -e "s/{{ GO_TODO_API_PORT }}/$GO_TODO_API_PORT/g" \
-      -e "s/{{ REACT_TODO_WEB_PORT }}/$REACT_TODO_WEB_PORT/g" \
+    -e "s/{{ REACT_TODO_WEB_PORT }}/$REACT_TODO_WEB_PORT/g" \
     < docker-compose-template.yml \
     > docker-compose.yml
