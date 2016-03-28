7a6b2dec74f187e92fcd92efe7ca83638f1dc27b
HuyNQ <huy@huynq.net>
2016-03-28 14:45:15 +0900

    Update docker config

--- init.sh
+++ init.sh
@@ -8,6 +8,8 @@ echo REACT_TODO_WEB_PORT=$REACT_TODO_WEB_PORT
 echo PHP_TODO_API_PORT=$PHP_TODO_API_PORT
 echo DB_PASSWORD=$DB_PASSWORD
 echo DB_NAME=$DB_NAME
+echo DB_USER=$DB_USER
+echo DB_PORT=$DB_PORT
 
 # find and replace
 sed -e "s/{{ HOST }}/$HOST/g" \
@@ -16,6 +18,8 @@ sed -e "s/{{ HOST }}/$HOST/g" \
     -e "s/{{ PHP_TODO_API_PORT }}/$PHP_TODO_API_PORT/g" \
     -e "s/{{ DB_PASSWORD }}/$DB_PASSWORD/g" \
     -e "s/{{ DB_NAME }}/$DB_NAME/g" \
+    -e "s/{{ DB_USER }}/$DB_USER/g" \
+    -e "s/{{ DB_PORT }}/$DB_PORT/g" \
     < docker-compose-template.yml \
     > docker-compose.yml
 
